8f30e73a7fa9fc306ff89c4f6944b05d5c6fff38
dobeid <dialajo9@gmail.com>
2024-02-29 19:41:46 -0800

    packages created and array of wuestions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this project?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can others contribute to your project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How can users test your project?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['None', 'MIT', 'Apache 2.0', 'GPL 3.0'],
+    filter: function(val) {
+      return val === 'None' ? '' : val; // Return an empty string if 'None' is selected
+    }
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) throw err;
+      console.log('README.md has been generated!');
+    });
+  }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateMarkdown(answers);
+    writeToFile('README.md', readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
